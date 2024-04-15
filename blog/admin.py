1fa27b8324b2fa37dbb79b077c65ce1b5b084351
KANDANELLY REDDY <h20220102@pilani.bits-pilani.ac.in>
2024-04-15 10:04:07 +0000

    Finish generic relationships

--- blog/admin.py
+++ blog/admin.py
@@ -4,5 +4,6 @@ from blog.models import Tag, Post,Comment
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ('slug', 'published_at')
+admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
